791010dacddade88019bbcd85bea408ce19cd1a1
yanglei <yanglei@bytetrade.io>
2018-10-12 18:59:33 +0800

    Update

---
+++ BranchDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BranchDemo
+//
+//  Created by 杨杨 on 2018/10/12.
+//  Copyright © 2018年 yanglei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BranchDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BranchDemo
+//
+//  Created by 杨杨 on 2018/10/12.
+//  Copyright © 2018年 yanglei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
